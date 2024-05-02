6a46bf1a3abf8c34df23e289e3a54348f6ca92b7
halcyon-past <titanssuperior@gmail.com>
2024-05-02 14:47:50 +0530

    Updated Query

--- app.py
+++ app.py
@@ -39,8 +39,10 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, 
+    if the question is creative but can be deciphered from the given context then create the answer and provide
+    it to the user, if the answer is not in provided context just say, "answer is not available in the context", 
+    don't provide the wrong answer no matter what.\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
